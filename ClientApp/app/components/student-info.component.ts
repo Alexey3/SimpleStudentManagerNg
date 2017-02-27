86fb5ae7af78448a006cbdcb6da004d45ab2c461
Alexey <Alexey@DESKTOP-78PNSV3>
2017-02-27 14:19:04 -0800

    change to split first last name on the info view

--- ClientApp/app/components/student-info.component.ts
+++ ClientApp/app/components/student-info.component.ts
@@ -9,6 +9,8 @@ import { StudentDataService, Student } from '../services/student-data.service'
 export class StudentInfoComponent implements OnInit{
     student = new Student();
     errorMessage: string;
+    firstName: string;
+    lastName: string;
 
     constructor(
         private route: ActivatedRoute,
@@ -22,7 +24,15 @@ export class StudentInfoComponent implements OnInit{
     getStudent(id: number) {
         this.service.getStudent(id)
             .subscribe(
-                result => { this.student = result },
+                result => { 
+                    this.student = result;
+                    var sa = this.student.name.trim().split(' ');
+                    if (sa.length > 0)
+                        this.firstName = sa[0];
+                    if (sa.length > 1) {
+                        this.lastName = sa.splice(1).map(o => o).join(' ');
+                    }
+                },
                 error => { this.errorMessage = error }
             );
     }
